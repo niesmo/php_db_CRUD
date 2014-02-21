f894e4dbf9c24fd5ad9a84176d54d8ac7e016b66
Nima Esmaili Mokaram <esmailimokaram.1@osu.edu>
2014-02-21 10:54:32 -0500

    Update Database_class.php

--- Database_class.php
+++ Database_class.php
@@ -1,73 +1,111 @@
-//Author Nima Esmaili Mokaram
 <?
+/*
+  Copyright (c) 2013 Nima Esmaili Mokaram
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+*/
+
 
 class Database {
 	private $lastResult;
 	private $connection;
-    	private $debug = true;
+	private $debug = false;
+	private $error = false;
 	
-	public function __construct($host, $user, $pass, $db ) {
+	public function __construct($host, $user, $pass, $db) {
 		$this->lastResult = NULL;
-		$this->connection = mysqli_connect ( $host, $user, $pass, $db, false, "/media/sdp1/home/masterme120/private/mysql/socket" ) or die ( 'HOST ERROR.' );
+		$this->connection = new mysqli ( $host, $user, $pass, $db ) or die ( 'HOST ERROR.' );
 		mysqli_select_db ( $this->connection, $db ) or die ( 'DATABASE ERROR.' );
 	}
 	
 	public function isConnected() {
 		return $this->connection;
 	}
-	private function tableExists($tableName) {
-		$this->query ( "SHOW TABLES LIKE '$tableName'" );
-		$table_exists = $this->numRows () != 0;
-		return $table_exists;
+	
+	public function error_on(){
+		$this->error = true;
 	}
+	public function error_off(){
+		$this->error = false;
+	}
+	
+	public function debug_on(){
+		$this->debug = true;
+	}
+	public function debug_off(){
+		$this->debug = false;
+	}
+	
 	public function query($query) {
-		//echo "this is the last query : $query <br />";
+		if($this->debug)
+			echo $query . "<br>";
 		$this->lastResult = mysqli_query ( $this->connection, $query );
-                if ($this->lastResult == false && $this->debug)
+                if ($this->lastResult == false && $this->error)
                     echo "$query is a BAD QUERY!!!";
 		return $this->lastResult;
 	}
 	
-	/*the difference between execute and query is that query saves the last query
-	*but execute does not*/
-	public function execute($query) {
-		return mysqli_query ( $this->connection, $query );
+	public function escapeBadChars($str){
+		return mysqli_real_escape_string ($this->connection , (string)$str);
+        
 	}
-	
-	/* this function takes the name of the table, an array of the name of the fields that you want to insert
-        * and also an array of values that you want to insert and insert it to the table*/
 	
 	public function insert($tableName, $columns, $values, $where = "") {
 		$q = "INSERT INTO $tableName ($columns) VALUES ($values)";
-		//echo $q;
 		if ($where != "")
 			$q .= "WHERE $where";
+			
+		if($this->debug)
+			echo $q . "<br>";
 		$this->query ( $q );
-		return mysqli_affected_rows ( $this->connection );
+		if(mysqli_affected_rows ( $this->connection ) != -1)
+			return true;
+		return false;
+	}
+	
+	public function delete($tableName, $where = "") {
+		$q = "DELETE FROM $tableName WHERE $where";
+		if($this->debug)
+			echo $q . "<br>";
+		$this->query ( $q );
+		if(mysqli_affected_rows ( $this->connection ) != -1)
+			return true;
+		return false;
 	}
 	
 	public function update($tableName, $set, $where="")
 	{
 		$q = "UPDATE $tableName SET $set WHERE $where";
-		//echo $q . "<br />";
+		if($this->debug)
+			echo $q . "<br>";
 		$this->query ( $q );
-		return mysqli_affected_rows ( $this->connection );
-	}
-    
-	public function delete($tableName, $where = "") {
-		$q = "DELETE FROM $tableName WHERE $where";
-		//echo "<h1>$q</h1>";
-		$this->query ( $q );
-		return mysqli_affected_rows ( $this->connection );
+		if(mysqli_affected_rows ( $this->connection ) != -1)
+			return true;
+		return false;
 	}
 	
-	public function select($tablesNames = "", $fields = "", $where = "", $groupby = "" , $sortby = "", $limit = "" ) {
+    public function select($tablesNames = "", $fields = "", $where = "", $groupby = "" , $sortby = "", $limit = "" ) {
 		$q = "SELECT " . ($fields == "" ? "*" : "$fields ") . ($tablesNames == "" ? "" : "FROM $tablesNames ") . ($where == "" ? "" : "WHERE $where ") . ($groupby == "" ? "" : " GROUP BY $groupby ") . ($sortby == "" ? "" : " ORDER BY $sortby ") . ($limit == "" ? "" : " LIMIT $limit ") ;
-		//echo $q . "<br />";
+		if($this->debug)
+			echo $q . "<br>";
+			
 		$this->query ($q);
-                
- //             return mysqli_fetch_array(MYSQLI_ASSOC);
-		$a = array();
 		while ( $entry = $this->fetchNextObject () )
 			$a [] = $entry;
 		return $a;
@@ -80,16 +118,16 @@ class Database {
 		if ($result == NULL || mysqli_num_rows ( $result ) < 1)
 			return NULL;
 		else
-            	return mysqli_fetch_assoc($result);
-			//return mysqli_fetch_object ( $result );
+			return mysqli_fetch_assoc($result);
 	}
-	
 	
 	public function affectedRows($result = NULL) {
 		if ($result == NULL)
 			return mysqli_affected_rows ();
 		return mysqli_affected_rows ();
 	}
+	
+	
 	public function numRows($result = NULL) {
 		if ($result == NULL)
 			return mysqli_num_rows ( $this->lastResult );
@@ -99,13 +137,11 @@ class Database {
 	
 	public function lastInsertedId() {
 		return mysqli_insert_id ($this->connection);
-		//return $connection.insert_id;
 	}
 	
-	function __destruct() {
+	public function __destruct() {	
 		mysqli_close ($this->connection);
-        	$this->connection = FALSE;
-		
+		$this->connection = FALSE;
 	}
 }
 ?>
